618ccf029f5db0e262c1a4a5357074dc5d8ddbab
michaelpair95 <michaelpair95@users.noreply.github.com>
2019-11-22 23:27:26 -0800

    adding the venv and other stuffs

--- stumblestream/streampicker/views.py
+++ stumblestream/streampicker/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello! Stumblesteam is in the works and coming soon!!!")
+    return HttpResponse("Stumblestream bby")
+    #return render(requst, 'index.html', {'name':'stumblestream!!!'})
