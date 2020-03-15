2c0d31e3ff8c6ec5af7640320a3df201e838be3a
michaelpair95 <michaelpair95@users.noreply.github.com>
2020-03-14 17:31:47 -0700

    add terraform stuff

---
+++ terraform/Main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = var.region
+}
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "tf-test-name"
+  application         = "${aws_elastic_beanstalk_application.tftest.name}"
+  solution_stack_name = "64bit Amazon Linux 2 v0.1.0 running Python 3.7 (BETA)"
+}
