f5914a67e05f488c395ca6b3d8fd251e118cf53b
michaelpair95 <michaelpair95@users.noreply.github.com>
2020-04-15 16:04:59 -0700

    added Iam name etc
    yeah, a push!

--- terraform/Main.tf
+++ terraform/Main.tf
@@ -1,13 +1,47 @@
 provider "aws" {
-  region     = var.region
+    region     = var.region
 }
 resource "aws_elastic_beanstalk_application" "tftest" {
-  name        = "tf-test-name"
-  description = "tf-test-desc"
+    name        = "tf-test-name"
+    description = "tf-test-desc"
 }
 
 resource "aws_elastic_beanstalk_environment" "tfenvtest" {
-  name                = "tf-test-name"
-  application         = "${aws_elastic_beanstalk_application.tftest.name}"
-  solution_stack_name = "64bit Amazon Linux 2 v0.1.0 running Python 3.7 (BETA)"
+    name                = "tf-test-name"
+    application         = "${aws_elastic_beanstalk_application.tftest.name}"
+    solution_stack_name = "64bit Amazon Linux 2 v0.1.0 running Python 3.7 (BETA)"
+
+    setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name = "IamInstanceProfile"
+        value = "aws-elasticbeanstalk-ec2-role"
+    }
+
 }
+
+
+# resource "aws_iam_instance_profile" "test_profile" {
+#   name = "test_profile"
+#   role = "${aws_iam_role.role.name}"
+# }
+
+# resource "aws_iam_role" "role" {
+#   name = "test_role"
+#   path = "/"
+
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Action": "sts:AssumeRole",
+#             "Principal": {
+#                "Service": "ec2.amazonaws.com"
+#             },
+#             "Effect": "Allow",
+#             "Sid": ""
+#         }
+#     ]
+# }
+# EOF
+# }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 variable "region" {
-  default = "us-west-1"
+  default = "us-west-2"
 }
